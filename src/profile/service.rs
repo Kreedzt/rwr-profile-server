19e864cd42ae6cc5b8d75adbf497a192a928490f
Kreedzt <zhaozisong1@live.com>
2022-02-08 15:41:28 +0800

    ✨  (profile) add donwload fn

--- src/profile/service.rs
+++ src/profile/service.rs
@@ -1,12 +1,14 @@
 use crate::{model::ResponseJson, profile::extract::extract_profile, AppData};
-use actix_web::{get, post, web, HttpResponse, Responder};
+use actix_web::{get, post, web, Result, HttpResponse, Responder};
 use tracing::{error, info, instrument};
+use actix_files::{NamedFile};
 
 pub fn profile_config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/profile")
             .service(query_profile)
-            .service(update_profile),
+            .service(update_profile)
+            .service(download_profile),
     );
 }
 
@@ -33,4 +35,23 @@ async fn query_profile(config: web::Data<AppData>, id: web::Path<(u64,)>) -> imp
 #[post("/update")]
 async fn update_profile() -> impl Responder {
     HttpResponse::Ok().body("update profile")
+}
+
+#[instrument]
+#[get("/download/{id}")]
+async fn download_profile(config: web::Data<AppData>, id: web::Path<(u64,)>) -> Result<NamedFile> {
+    info!("");
+
+    let id: u64 = id.into_inner().0;
+    let path = format!("{}/{}.profile", &config.rwr_profile_folder_path, id);
+
+    Ok(NamedFile::open(path).map_err(|err| {
+        let err_msg = format!("download {} profile error: {}", id, err.to_string());
+        error!("{}", err_msg);
+
+        let custom_err = ResponseJson::default().set_err_msg(&err_msg);
+
+        HttpResponse::BadRequest()
+            .json(custom_err)
+    })?)
 }
